3cfc6da15ff74e35e1591184f4dd786e177dbb56
Daniel Laderman <danielladerman@Mac-862.lan>
2025-08-28 00:08:11 -0400

    fix(frontend): force-add utils index.js for Vercel resolver

---
+++ frontend_base44/src/lib/utils/index.js
@@ -0,0 +1,11 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+
+export function createPageUrl(pageName) {
+    return '/' + pageName.toLowerCase().replace(/ /g, '-');
+}
