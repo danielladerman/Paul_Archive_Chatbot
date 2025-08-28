8393615de4226beb4815a01706808374d91901d6
Daniel Laderman <danielladerman@Mac-862.lan>
2025-08-27 23:07:44 -0400

    fix: convert utils file to plain javascript

--- frontend_base44/src/utils/index.js
+++ frontend_base44/src/utils/index.js
@@ -1,11 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
 
-export function createPageUrl(pageName: string) {
+export function createPageUrl(pageName) {
     return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
